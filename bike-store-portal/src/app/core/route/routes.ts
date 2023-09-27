ccee03dc4c138fc4fcb7bb6b51bfff5b1e80244a
trongvuong0108 <tronghandsome1111@gmail.com>
2023-09-28 06:23:56 +0700

    update new functions

---
+++ bike-store-portal/src/app/core/route/routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from "@angular/router";
+import { HomeComponent } from "src/app/module/home/home.component";
+
+export const routes : Routes = [
+  { path: 'Home', component: HomeComponent },
+];
